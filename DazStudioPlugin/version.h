f0af5e479970f9cb25564d1f47a1b362ea1e8c09
danielbui78 <danielbui78@gmail.com>
2022-05-20 15:31:02 -0400

    Update version.h

--- DazStudioPlugin/version.h
+++ DazStudioPlugin/version.h
@@ -4,7 +4,7 @@
 // Version number for DazToUnreal
 #define PLUGIN_MAJOR	2
 #define PLUGIN_MINOR	0
-#define PLUGIN_REV		13
-#define PLUGIN_BUILD	4
+#define PLUGIN_REV		15
+#define PLUGIN_BUILD	2
 
 #define PLUGIN_VERSION	DZ_MAKE_VERSION( PLUGIN_MAJOR, PLUGIN_MINOR, PLUGIN_REV, PLUGIN_BUILD )
