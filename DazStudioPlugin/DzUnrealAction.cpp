a2f7cc7acc6eca7e41afda44926f35730eccfb83
danielbui78 <danielbui78@gmail.com>
2025-10-12 17:05:31 -0700

    g1-9 for ue5 update

--- DazStudioPlugin/DzUnrealAction.cpp
+++ DazStudioPlugin/DzUnrealAction.cpp
@@ -745,9 +745,12 @@ bool DzUnrealAction::postProcessFbx(QString fbxFilePath)
 		}
 
 		QStringList aArchiveFilenameList;
-		aArchiveFilenameList << "/g9_unreal_apose_fixed.fbx";
-		aArchiveFilenameList << "/g1_unreal_apose_fixed.fbx";
 		aArchiveFilenameList << "/ue5_apose.fbx";
+		aArchiveFilenameList << "/g9_ue5_apose.fbx";
+		aArchiveFilenameList << "/g8_ue5_apose.fbx";
+		aArchiveFilenameList << "/g3_ue5_apose.fbx";
+		aArchiveFilenameList << "/g2_ue5_apose.fbx";
+		aArchiveFilenameList << "/g1_ue5_apose.fbx";
 		foreach(QString sArchiveFilename, aArchiveFilenameList)
 		{
 			QString sEmbeddedArchivePath = m_sEmbeddedFolderPath + sArchiveFilename;
