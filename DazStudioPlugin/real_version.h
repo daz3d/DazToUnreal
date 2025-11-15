a322571b33037a1552d332404fd0c23cfc777c29
danielbui78 <danielbui78@gmail.com>
2025-11-15 11:08:14 -0800

    bugfix genesis 9 bake pivots broken

--- DazStudioPlugin/real_version.h
+++ DazStudioPlugin/real_version.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#define PRODUCT_VERSION_STRING "2025 version 4.40 alpha"
+#define PRODUCT_VERSION_STRING "2025 version 4.44 alpha"
 #define VER_MAJOR	2025
 #define VER_MINOR	4
-#define VER_REV		41
-#define VER_BUILD	245
+#define VER_REV		44
+#define VER_BUILD	255
 
 #define TOSTRING(x) #x
 #define VERSION_STRING	TOSTRING(VER_MAJOR) "." TOSTRING(VER_MINOR) "." TOSTRING(VER_REV) "." TOSTRING(VER_BUILD)
